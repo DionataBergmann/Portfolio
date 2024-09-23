826266812e4f8c52769b87b290062cdc377bc637
DionataBergmann <dionata.podewils@outlook.com>
2024-09-23 14:44:29 -0300

    :art: Add section observer to scroll flow

---
+++ src/hooks/useSectionObserver/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+const useSectionObserver = (setActiveButton: (id: string) => void) => {
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5, 
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveButton(entry.target.id);
+        }
+      });
+    }, options);
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        observer.unobserve(section);
+      });
+    };
+  }, [setActiveButton]);
+};
+
+export default useSectionObserver;
